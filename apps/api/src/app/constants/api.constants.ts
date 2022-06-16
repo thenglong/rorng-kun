091cd60f7bf3769cc98700c2ba301447f419871e
Thenglong Heng <thenglongheng@Thenglongs-MacBook-Pro.local>
2022-06-16 17:05:12 +0700

    chore( remove api and it's dependency

--- apps/api/src/app/constants/api.constants.ts
+++
@@ -1,2 +0,0 @@
-export const API_VERSION = "v1"
-export const API_BASE_URL = `/api/${API_VERSION}`
--- apps/api/src/app/middlewares/incoming-request.middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from "express"
-
-export const logIncomingRequest = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  next()
-}
